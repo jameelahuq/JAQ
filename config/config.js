0f568757a7f96f310aaf237931b85c2aebbb2028
Jameela Huq <jameela.huq@gmail.com>
2015-11-10 23:03:12 -0800

    changed to https

--- config/config.js
+++ config/config.js
@@ -7,6 +7,6 @@ module.exports = {
   'googleAuth' : {
     'clientID'      : '825949035576-t22hs85kirk3emo4blk7rscr4onc73mr.apps.googleusercontent.com', // your App ID
     'clientSecret'  : 'W0u52t-1lPv8tUs_Rrdz_loY', // your App Secret
-    'callbackURL'   : 'http://jaqd.herokuapp.com/auth/google/callback'
+    'callbackURL'   : 'https://jaqd.herokuapp.com/auth/google/callback'
   }
 };
