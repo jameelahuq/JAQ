fa28e8cb7f1122aca5fcbc79223059cc0d6b9310
Jameela Huq <jameela.huq@gmail.com>
2015-11-12 04:43:31 -0800

    no local host....

--- config/config.js
+++ config/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  MONGO_URL:  process.env.MONGOLAB_URI || 'mongodb://localhost/JAQblogger',
+  MONGO_URL:  process.env.MONGOLAB_URI,
   SECRET: 'ANYFUCKINGTHINGIWANT',
   MAILGUN_KEY: process.env.MAILGUN_KEY,
   MAILGUN_DOMAIN: process.env.MAILGUN_DOMAIN,
@@ -8,7 +8,7 @@ module.exports = {
     'clientID'      : '825949035576-t22hs85kirk3emo4blk7rscr4onc73mr.apps.googleusercontent.com', // your App ID
     'clientSecret'  : 'W0u52t-1lPv8tUs_Rrdz_loY', // your App Secret
     //'callbackURL'   : 'https://jaqd.herokuapp.com/auth/google/callback'
-    'callbackURL'   : process.env.GOOGLE_CALLBACKURL || 'http://localhost:8080/auth/google/callback'
+    'callbackURL'   : process.env.GOOGLE_CALLBACKURL
     
   }
 };
