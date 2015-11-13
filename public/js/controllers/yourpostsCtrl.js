07de40fbc6c71fc6ea47ae9f7d949aaa03239ab6
qdumont <quadebdumont@gmail.com>
2015-11-12 23:33:58 -0800

    date format

--- public/js/controllers/yourpostsCtrl.js
+++ public/js/controllers/yourpostsCtrl.js
@@ -5,11 +5,10 @@ jaqApp.controller('yourpostsCtrl', function ($scope, $http) {
     $http({
       method: 'GET',
       url: '/users/posts'
-      //change the userID depending on signed in user
+        //change the userID depending on signed in user
     }).then(function successCallback(response) {
       $scope.posts = response.data
-    }, function errorCallback(response) {
-    });
+    }, function errorCallback(response) {});
   };
   get();
 
@@ -17,37 +16,36 @@ jaqApp.controller('yourpostsCtrl', function ($scope, $http) {
   $scope.delete = function (id) {
     var id = id;
     swal({
-          title: "Are you sure?",
-          text: "You will not be able to recover this imaginary file!",
-          type: "warning",
-          showCancelButton: true,
-          confirmButtonColor: "#DD6B55",
-          confirmButtonText: "Yes, delete it!",
-          cancelButtonText: "No, cancel plx!",
-          closeOnConfirm: false,
-          closeOnCancel: false
-        },
-        function (isConfirm) {
-          if (isConfirm) {
-            swal("Deleted!", "Your imaginary file has been deleted.", "success");
-            $http({
-              method: 'DELETE',
-              url: '/removePost/' + id
-            }).then(function successCallback(response) {
-              get();
-            }, function errorCallback(response) {
-            });
-          } else {
-            swal("Cancelled", "Your imaginary file is safe :)", "error");
-          }
-        });
+        title: "Are you sure?",
+        text: "You will not be able to recover this imaginary file!",
+        type: "warning",
+        showCancelButton: true,
+        confirmButtonColor: "#DD6B55",
+        confirmButtonText: "Yes, delete it!",
+        cancelButtonText: "No, cancel plx!",
+        closeOnConfirm: false,
+        closeOnCancel: false
+      },
+      function (isConfirm) {
+        if (isConfirm) {
+          swal("Deleted!", "Your imaginary file has been deleted.", "success");
+          $http({
+            method: 'DELETE',
+            url: '/removePost/' + id
+          }).then(function successCallback(response) {
+            get();
+          }, function errorCallback(response) {});
+        } else {
+          swal("Cancelled", "Your imaginary file is safe :)", "error");
+        }
+      });
 
     $http({
       method: 'DELETE',
       url: 'posts/removePost/' + id
 
       //change the userID depending on signed in user
-      }).then(function successCallback(response) {
+    }).then(function successCallback(response) {
       get();
     });
   };
@@ -56,15 +54,15 @@ jaqApp.controller('yourpostsCtrl', function ($scope, $http) {
   var currentEdit = [];
   $scope.edit = function (post) {
     currentEdit.push(post);
-    if (post){
-       $scope.editPost = post.thePost;
+    if (post) {
+      $scope.editPost = post.thePost;
     }
-    $scope.editText === true ? $scope.editText = false : $scope.editText = false 
+    $scope.editText === true ? $scope.editText = false : $scope.editText = false
     var number = currentEdit.length - 1
-    if (currentEdit[number] !== undefined){
+    if (currentEdit[number] !== undefined) {
       var currentEditId = currentEdit[number]._id;
     }
-    $scope.pushEdit = function(editPost){
+    $scope.pushEdit = function (editPost) {
       var obj = {
         thePost: editPost
       }
@@ -73,17 +71,10 @@ jaqApp.controller('yourpostsCtrl', function ($scope, $http) {
         method: 'POST',
         url: 'posts/edit/' + currentEditId,
         data: obj
-        //change the userID depending on signed in user
+          //change the userID depending on signed in user
       }).then(function successCallback(response) {
         get();
       });
     }
   }
-  
-//  $scope.update = function (editPost) {
-//    
-//    console.log(editPost)
-//    
-//  }
 });
-
