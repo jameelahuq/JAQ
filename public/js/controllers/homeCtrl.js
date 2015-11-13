9dbfd0207a78493df376055e2d44d42e5774d3e0
qdumont <quadebdumont@gmail.com>
2015-11-13 00:08:25 -0800

    version ready for heroku push

--- public/js/controllers/homeCtrl.js
+++ public/js/controllers/homeCtrl.js
@@ -2,28 +2,37 @@
 
 jaqApp.controller('homeCtrl', function ($scope, $http) {
 
-      var executed = false;
-      $(window).scroll(function() {   
-        if($(window).scrollTop() + $(window).height() > $(document).height() / 2 && executed === false && $scope.signedIn !== true) {
-          executed = true;
-          swal({   title: "Sign in or Continue Reading?",   text: "Sign in to share your opinion!",   showCancelButton: true, cancelButtonText:"Keep Reading",  imageUrl: "../../img/smiley-T-shirts.jpg", confirmButtonColor: "#00bb66",   confirmButtonText: "Yes, sign in!",   closeOnConfirm: false }, function(){
-            
-            swal("Awesome!", "You're signed in!", "success"); 
-            window.location="/auth/google"
-          });
-        }
+  var executed = false;
+  $(window).scroll(function () {
+    if ($(window).scrollTop() + $(window).height() > $(document).height() / 2 && executed === false && $scope.signedIn !== true) {
+      executed = true;
+      swal({
+        title: "Sign in or Continue Reading?",
+        text: "Sign in to share your opinion!",
+        showCancelButton: true,
+        cancelButtonText: "Keep Reading",
+        imageUrl: "../../img/smiley-T-shirts.jpg",
+        confirmButtonColor: "#00bb66",
+        confirmButtonText: "Yes, sign in!",
+        closeOnConfirm: false
+      }, function () {
+
+        swal("Awesome!", "You're signed in!", "success");
+        window.location = "/auth/google"
       });
-      
-      var something = (function() {
-        var executed = false;
-        return function () {
-          if (!executed) {
-            executed = true;
-            // do something
-          }
-        };
-      })();
-  
+    }
+  });
+
+  var something = (function () {
+    var executed = false;
+    return function () {
+      if (!executed) {
+        executed = true;
+        // do something
+      }
+    };
+  })();
+
 
 
   var get = function () {
@@ -37,101 +46,101 @@ jaqApp.controller('homeCtrl', function ($scope, $http) {
     }, function errorCallback(response) {});
   }
   get();
-  
-   $scope.clearField = function () {
-     $scope.comment = "";
-   }
-   $scope.liked = "Like";
-   var get = function () {
-     $http({
-       method: 'GET',
-       url: '/everything'
-         //change the userID depending on signed in user
-     }).then(function successCallback(response) {
+
+
+
+  $scope.liked = "Like";
+  var get = function () {
+    $http({
+      method: 'GET',
+      url: '/everything'
+        //change the userID depending on signed in user
+    }).then(function successCallback(response) {
 
       console.log(response);
 
-       $scope.data = response.data;
-     }, function errorCallback(response) {
+      $scope.data = response.data;
+    }, function errorCallback(response) {
       console.log(response)
-     });
-   }
-   get();
+    });
+  }
+  get();
 
-   $scope.like = function (id) {
-     
-     if ($scope.liked === "Dislike") {
-       $http({
-         method: 'POST',
+  $scope.like = function (id) {
 
-         url: '/posts/dislikedPost/' + id
+    if ($scope.liked === "Dislike") {
+      $http({
+        method: 'POST',
 
-       }).then(function successCallback(response) {
-         $scope.liked = "Like";
-         get();
-       }, function errorCallback(response) {});
-     } else {
-       $http({
-         method: 'POST',
-         url: 'posts/likedPost/' + id
+        url: '/posts/dislikedPost/' + id
 
-       }).then(function successCallback(response) {
-         $scope.liked = "Dislike";
-         get();
-       }, function errorCallback(response) {});
-     }
-   }
+      }).then(function successCallback(response) {
+        $scope.liked = "Like";
+        get();
+      }, function errorCallback(response) {});
+    } else {
+      $http({
+        method: 'POST',
+        url: 'posts/likedPost/' + id
 
-   var getComments = function () {
-     $scope.getComments = function (id) {
-       $http({
+      }).then(function successCallback(response) {
+        $scope.liked = "Dislike";
+        get();
+      }, function errorCallback(response) {});
+    }
+  }
 
-         method: 'GET',
-         url: 'posts/comments/' + id
+  var getComments = function () {
+    $scope.getComments = function (id) {
+      $http({
 
-       }).then(function successCallback(response) {
+        method: 'GET',
+        url: 'posts/comments/' + id
 
-         console.log("comments", response);
-         $scope.comments = response.data.comments
+      }).then(function successCallback(response) {
 
-       }, function errorCallback(response) {});
-       $scope.addComment = function (comment) {
-         var comment = comment;
-         $http({
-           method: 'POST',
-           data: comment,
-           url: 'comments/addComment/' + id
-         }).then(function successCallback(response) {
-           $scope.getComments(id)
-         }, function errorCallback(response) {
-           swal("Oops...", "Please Sign in to Comment", "error")
-         });
-       }
-     }
+        console.log("comments", response);
+        $scope.comments = response.data.comments
 
-   }
-   getComments();
+      }, function errorCallback(response) {});
+      $scope.addComment = function (comment) {
+        var comment = comment;
+        $http({
+          method: 'POST',
+          data: comment,
+          url: 'comments/addComment/' + id
+        }).then(function successCallback(response) {
+          $scope.getComments(id)
+                    
+        }, function errorCallback(response) {
+          swal("Oops...", "Please Sign in to Comment", "error")
+        });
+      }
+    }
 
-   $scope.box1 = true;
- });
+  }
+  getComments();
 
- jaqApp.directive('slideToggle', function () {
-   return {
-     restrict: 'A',
-     scope: {
-       isOpen: "=slideToggle" // 'data-slide-toggle' in our html
-     },
-     link: function (scope, element, attr) {
-       var slideDuration = parseInt(attr.slideToggleDuration, 10) || 200;
+  $scope.box1 = true;
+});
 
-       // Watch for when the value bound to isOpen changes
-       // When it changes trigger a slideToggle
-       scope.$watch('isOpen', function (newIsOpenVal, oldIsOpenVal) {
-         if (newIsOpenVal !== oldIsOpenVal) {
-           element.stop().slideToggle(slideDuration);
-         }
-       });
+jaqApp.directive('slideToggle', function () {
+  return {
+    restrict: 'A',
+    scope: {
+      isOpen: "=slideToggle" // 'data-slide-toggle' in our html
+    },
+    link: function (scope, element, attr) {
+      var slideDuration = parseInt(attr.slideToggleDuration, 10) || 200;
 
-     }
-   };
- });
+      // Watch for when the value bound to isOpen changes
+      // When it changes trigger a slideToggle
+      scope.$watch('isOpen', function (newIsOpenVal, oldIsOpenVal) {
+        if (newIsOpenVal !== oldIsOpenVal) {
+          element.stop().slideToggle(slideDuration);
+        }
+      });
+
+    }
+  };
+});
